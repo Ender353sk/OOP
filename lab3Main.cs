8404b5275008417592bd9740e7760e0596498254
Ender353sk <ender3597@gmail.com>
2025-03-14 12:10:46 +0200

    Update lab3Main.cs

--- lab3Main.cs
+++ lab3Main.cs
@@ -2,9 +2,9 @@ using System;
 
 public class Rectangle
 {
-    private double x1, y1, x2, y2; // Координаты вершин
+    private double x1, y1, x2, y2; // Координати вершин
 
-    // Конструктор по умолчанию
+    // Конструктор за замовчуванням
     public Rectangle()
     {
         x1 = 0;
@@ -13,7 +13,7 @@ public class Rectangle
         y2 = 1;
     }
 
-    // Конструктор с параметрами
+    // Конструктор з параметрами
     public Rectangle(double x1, double y1, double x2, double y2)
     {
         this.x1 = x1;
@@ -22,7 +22,7 @@ public class Rectangle
         this.y2 = y2;
     }
 
-    // Конструктор копирования
+    // Конструктор копіювання
     public Rectangle(Rectangle rect)
     {
         x1 = rect.x1;
@@ -31,31 +31,31 @@ public class Rectangle
         y2 = rect.y2;
     }
 
-    // Метод вычисления площади
+    // Метод обчислення площі
     public double Area()
     {
         return Math.Abs((x2 - x1) * (y2 - y1));
     }
 
-    // Метод вычисления периметра
+    // Метод обчислення периметру
     public double Perimeter()
     {
         return 2 * (Math.Abs(x2 - x1) + Math.Abs(y2 - y1));
     }
 
-    // Метод получения данных прямоугольника
+    // Метод отримання даних прямокутника
     public void GetCoordinates()
     {
         Console.WriteLine($"Вершина 1: ({x1}, {y1}), Вершина 2: ({x2}, {y2})");
     }
 
-    // Перегрузка оператора вычитания
+    // Перевантаження оператора віднімання
     public static Rectangle operator -(Rectangle rect1, Rectangle rect2)
     {
         return new Rectangle(rect1.x1 - rect2.x1, rect1.y1 - rect2.y1, rect1.x2 - rect2.x2, rect1.y2 - rect2.y2);
     }
 
-    // Перегрузка оператора деления
+    // Перевантаження оператора ділення
     public static Rectangle operator /(Rectangle rect, double value)
     {
         return new Rectangle(rect.x1 / value, rect.y1 / value, rect.x2 / value, rect.y2 / value);
@@ -66,26 +66,26 @@ public class Program
 {
     public static void Main()
     {
-        // Создание объектов Q1, Q2, Q3 с использованием различных конструкторов
+        // Створення об'єктів Q1, Q2, Q3 з використанням різних конструкторів
         Rectangle Q1 = new Rectangle();
         Rectangle Q2 = new Rectangle(2, 2, 4, 4);
         Rectangle Q3 = new Rectangle(Q2);
 
-        // "Уменьшение" объекта Q2 в 2 раза
+        // "Зменшення" об'єкта Q2 в 2 рази
         Q2 = Q2 / 2;
 
-        // "Вычитание" из объекта Q3 объекта Q2 и результат "помещаем" в объект Q1
+        // "Віднімання" з об'єкта Q3 об'єкта Q2 і результат "поміщаємо" в об'єкт Q1
         Q1 = Q3 - Q2;
 
-        // Вывод результатов на экран
+        // Виведення результатів на екран
         Console.WriteLine("Q1: ");
         Q1.GetCoordinates();
-        Console.WriteLine($"Площадь Q1: {Q1.Area()}, Периметр Q1: {Q1.Perimeter()}");
+        Console.WriteLine($"Площа Q1: {Q1.Area()}, Периметр Q1: {Q1.Perimeter()}");
         Console.WriteLine("Q2: ");
         Q2.GetCoordinates();
-        Console.WriteLine($"Площадь Q2: {Q2.Area()}, Периметр Q2: {Q2.Perimeter()}");
+        Console.WriteLine($"Площа Q2: {Q2.Area()}, Периметр Q2: {Q2.Perimeter()}");
         Console.WriteLine("Q3: ");
         Q3.GetCoordinates();
-        Console.WriteLine($"Площадь Q3: {Q3.Area()}, Периметр Q3: {Q3.Perimeter()}");
+        Console.WriteLine($"Площа Q3: {Q3.Area()}, Периметр Q3: {Q3.Perimeter()}");
     }
 }
